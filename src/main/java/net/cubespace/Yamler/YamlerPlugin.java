abc182aa9f1f2669e93f302e50a7552725c0c5b0
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-01-26 17:51:25 +0100

    Remove the Test Config

--- src/main/java/net/cubespace/Yamler/YamlerPlugin.java
+++ src/main/java/net/cubespace/Yamler/YamlerPlugin.java
@@ -1,6 +1,5 @@
 package net.cubespace.Yamler;
 
-import net.cubespace.Yamler.Config.InvalidConfigurationException;
 import net.md_5.bungee.api.plugin.Plugin;
 
 import java.io.IOException;
@@ -10,13 +9,6 @@ import java.io.IOException;
  */
 public class YamlerPlugin extends Plugin {
     public void onEnable() {
-        TestConfig testConfig = new TestConfig(this);
-        try {
-            testConfig.init();
-        } catch (InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
-
         try {
             Metrics metrics = new Metrics(this);
             metrics.start();
