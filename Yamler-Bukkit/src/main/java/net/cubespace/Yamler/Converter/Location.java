76fa7acab26e085bf1ea9cf0831a114460c048f8
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2014-08-24 10:41:38 +0200

    Fixed double casting to floats

--- Yamler-Bukkit/src/main/java/net/cubespace/Yamler/Converter/Location.java
+++ Yamler-Bukkit/src/main/java/net/cubespace/Yamler/Converter/Location.java
@@ -13,24 +13,25 @@ import java.util.Map;
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
 public class Location implements Converter {
-    public Location(InternalConverter converter) {}
+    public Location( InternalConverter converter ) {
+    }
 
     @Override
-    public Object toConfig(Class<?> type, Object obj, ParameterizedType genericType) throws Exception {
+    public Object toConfig( Class<?> type, Object obj, ParameterizedType genericType ) throws Exception {
         org.bukkit.Location location = (org.bukkit.Location) obj;
         Map<String, Object> saveMap = new HashMap<>();
-        saveMap.put("world", location.getWorld().getName());
-        saveMap.put("x", location.getX());
-        saveMap.put("y", location.getY());
-        saveMap.put("z", location.getZ());
-        saveMap.put("yaw", location.getYaw());
-        saveMap.put("pitch", location.getPitch());
+        saveMap.put( "world", location.getWorld().getName() );
+        saveMap.put( "x", location.getX() );
+        saveMap.put( "y", location.getY() );
+        saveMap.put( "z", location.getZ() );
+        saveMap.put( "yaw", location.getYaw() );
+        saveMap.put( "pitch", location.getPitch() );
 
         return saveMap;
     }
 
     @Override
-    public Object fromConfig(Class type, Object section, ParameterizedType genericType) throws Exception {
+    public Object fromConfig( Class type, Object section, ParameterizedType genericType ) throws Exception {
         Map<String, Object> locationMap;
         if ( section instanceof Map ) {
             locationMap = (Map<String, Object>) section;
@@ -38,12 +39,33 @@ public class Location implements Converter {
             locationMap = (Map<String, Object>) ( (ConfigSection) section ).getRawMap();
         }
 
-        return new org.bukkit.Location(Bukkit.getWorld((String) locationMap.get("world")), (Double) locationMap.get("x"), (Double) locationMap.get("y"), (Double) locationMap.get("z"), (Float) locationMap.get("yaw"), (Float) locationMap.get("pitch") );
+        Float yaw;
+        if ( locationMap.get( "yaw" ) instanceof Double ) {
+            Double dYaw = (Double) locationMap.get( "yaw" );
+            yaw = dYaw.floatValue();
+        } else {
+            yaw = (Float) locationMap.get( "yaw" );
+        }
+
+        Float pitch;
+        if ( locationMap.get( "pitch" ) instanceof Double ) {
+            Double dPitch = (Double) locationMap.get( "pitch" );
+            pitch = dPitch.floatValue();
+        } else {
+            pitch = (Float) locationMap.get( "pitch" );
+        }
+
+        return new org.bukkit.Location( Bukkit.getWorld( (String) locationMap.get( "world" ) ),
+                (Double) locationMap.get( "x" ),
+                (Double) locationMap.get( "y" ),
+                (Double) locationMap.get( "z" ),
+                yaw,
+                pitch );
     }
 
     @Override
-    public boolean supports(Class<?> type) {
-        return org.bukkit.Location.class.isAssignableFrom(type);
+    public boolean supports( Class<?> type ) {
+        return org.bukkit.Location.class.isAssignableFrom( type );
     }
 
 }
